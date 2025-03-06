89f83772d29f8a086d76e4b1f4aa3366f38dcc22
Michael Getz <getzm@oregonstate.edu>
2025-03-06 18:26:40 -0500

    save before pull

--- listen/constant_send.py
+++ listen/constant_send.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import logging
 
 HUB_IP = "192.168.2.1"  # Change to your hub Pi’s IP
-TOPIC = "sensor/heartbeat"
+TOPIC = "heartbeat"
 SENSOR_NAME = "sensor_1"
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
